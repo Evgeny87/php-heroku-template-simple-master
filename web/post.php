344ffc6f1c77def38e1d60a6cbb835888ae4c6e8
Evgeny87 <sun_nik_off@mail.ru>
2023-11-23 20:30:04 +0300

    Update post.php

--- web/post.php
+++ web/post.php
@@ -1,4 +1,10 @@
 <?
+
+function redirectInIndexHtml(): void {
+    header("Location: index.php");
+    exit();
+}
+
 if(isset($_POST['login'])) {
     echo 'Нажата была кнопка с именем Войти';
 } elseif(isset($_POST['register'])) {
